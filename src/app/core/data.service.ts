d4e112cde1342fc23ed874678365ef90be4a870d
Andrew De Zoyza <andrewdezoyza@aizenit.com>
2023-04-25 20:05:27 +0530

    Transforming Data with RxJS

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, from } from 'rxjs';
+import {map , tap} from 'rxjs/operators';
+
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from "app/models/reader";
 import { Book } from "app/models/book";
-import { Observable } from 'rxjs';
 import { BookTrackerError } from 'app/models/bookTrackerError';
+import { OldBook } from 'app/models/oldBook';
 
 @Injectable({
   providedIn: 'root'
@@ -41,5 +44,16 @@ export class DataService {
         'Authorization': 'my-token'
       })
     });
+  }
+
+  getOldBookById(id:number) : Observable<OldBook>{
+    return this.http.get<Book>(`/api/books/${id}`)
+    .pipe(
+      map(b => <OldBook>{
+        bookTitle : b.title,
+        year: b.publicationYear
+      }),
+      tap(classicBook => console.log(classicBook))
+    );
   }
 }
