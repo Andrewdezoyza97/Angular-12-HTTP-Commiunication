da96246cc93c9dd4042d9f61b29453049d9db480
Andrew De Zoyza <andrewdezoyza@aizenit.com>
2023-04-27 16:09:11 +0530

    Using Metadata with interceptors

--- dist/main.js
+++ dist/main.js
@@ -274,23 +274,27 @@ AppModule = (0,tslib__WEBPACK_IMPORTED_MODULE_10__.__decorate)([
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "CONTENT_TYPE": () => (/* binding */ CONTENT_TYPE),
 /* harmony export */   "AddHeaderInterceptor": () => (/* binding */ AddHeaderInterceptor)
 /* harmony export */ });
-/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ 4762);
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ 7716);
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! tslib */ 4762);
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common/http */ 1841);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ 7716);
 
 
+
+const CONTENT_TYPE = new _angular_common_http__WEBPACK_IMPORTED_MODULE_0__.HttpContextToken(() => 'application/json');
 let AddHeaderInterceptor = class AddHeaderInterceptor {
     intercept(req, next) {
         console.log(`AddHeaderInterceptor - ${req.url}`);
         let jsonReq = req.clone({
-            setHeaders: { 'Content-Type': 'application/json' }
+            setHeaders: { 'Content-Type': req.context.get(CONTENT_TYPE) }
         });
         return next.handle(jsonReq);
     }
 };
-AddHeaderInterceptor = (0,tslib__WEBPACK_IMPORTED_MODULE_0__.__decorate)([
-    (0,_angular_core__WEBPACK_IMPORTED_MODULE_1__.Injectable)({
+AddHeaderInterceptor = (0,tslib__WEBPACK_IMPORTED_MODULE_1__.__decorate)([
+    (0,_angular_core__WEBPACK_IMPORTED_MODULE_2__.Injectable)({
         providedIn: 'root'
     })
 ], AddHeaderInterceptor);
@@ -429,15 +433,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "DataService": () => (/* binding */ DataService)
 /* harmony export */ });
-/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! tslib */ 4762);
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! @angular/core */ 7716);
-/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/common/http */ 1841);
-/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! rxjs */ 205);
-/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! rxjs/operators */ 5304);
-/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! rxjs/operators */ 8002);
-/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! rxjs/operators */ 8307);
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! tslib */ 4762);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! @angular/core */ 7716);
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common/http */ 1841);
+/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! rxjs */ 205);
+/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! rxjs/operators */ 5304);
+/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! rxjs/operators */ 8002);
+/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! rxjs/operators */ 8307);
 /* harmony import */ var app_data__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! app/data */ 8387);
 /* harmony import */ var app_models_bookTrackerError__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! app/models/bookTrackerError */ 5582);
+/* harmony import */ var _add_header_interceptor__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./add-header.interceptor */ 483);
+
 
 
 
@@ -462,8 +468,10 @@ let DataService = class DataService {
     //Retriving a Collection
     getAllBooks() {
         console.log('Getting sll books from the server.');
-        return this.http.get('/api/books')
-            .pipe((0,rxjs_operators__WEBPACK_IMPORTED_MODULE_2__.catchError)(err => this.handleHttpError(err)));
+        return this.http.get('/api/books', {
+            context: new _angular_common_http__WEBPACK_IMPORTED_MODULE_3__.HttpContext().set(_add_header_interceptor__WEBPACK_IMPORTED_MODULE_2__.CONTENT_TYPE, 'application/xml')
+        })
+            .pipe((0,rxjs_operators__WEBPACK_IMPORTED_MODULE_4__.catchError)(err => this.handleHttpError(err)));
     }
     //Handling Http Errors
     handleHttpError(error) {
@@ -471,12 +479,12 @@ let DataService = class DataService {
         dataError.errorNumber = 100;
         dataError.message = error.statusText;
         dataError.friendlyMessage = 'An error Occured retrieving data.';
-        return (0,rxjs__WEBPACK_IMPORTED_MODULE_3__.throwError)(dataError);
+        return (0,rxjs__WEBPACK_IMPORTED_MODULE_5__.throwError)(dataError);
     }
     //Retrvieving a Single Item
     getBookById(id) {
         return this.http.get(`/api/books/${id}`, {
-            headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_4__.HttpHeaders({
+            headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_3__.HttpHeaders({
                 'Accept': 'application/json',
                 'Authorization': 'my-token'
             })
@@ -484,15 +492,15 @@ let DataService = class DataService {
     }
     getOldBookById(id) {
         return this.http.get(`/api/books/${id}`)
-            .pipe((0,rxjs_operators__WEBPACK_IMPORTED_MODULE_5__.map)(b => ({
+            .pipe((0,rxjs_operators__WEBPACK_IMPORTED_MODULE_6__.map)(b => ({
             bookTitle: b.title,
             year: b.publicationYear
-        })), (0,rxjs_operators__WEBPACK_IMPORTED_MODULE_6__.tap)(classicBook => console.log(classicBook)));
+        })), (0,rxjs_operators__WEBPACK_IMPORTED_MODULE_7__.tap)(classicBook => console.log(classicBook)));
     }
     //Insert
     addBook(newBook) {
         return this.http.post('/api/books', newBook, {
-            headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_4__.HttpHeaders({
+            headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_3__.HttpHeaders({
                 'Content-Type': 'application/json'
             })
         });
@@ -500,7 +508,7 @@ let DataService = class DataService {
     //Update
     updateBook(updatedBook) {
         return this.http.put(`/api/books/${updatedBook.bookID}`, updatedBook, {
-            headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_4__.HttpHeaders({
+            headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_3__.HttpHeaders({
                 'Content-Type': 'application/json'
             })
         });
@@ -511,10 +519,10 @@ let DataService = class DataService {
     }
 };
 DataService.ctorParameters = () => [
-    { type: _angular_common_http__WEBPACK_IMPORTED_MODULE_4__.HttpClient }
+    { type: _angular_common_http__WEBPACK_IMPORTED_MODULE_3__.HttpClient }
 ];
-DataService = (0,tslib__WEBPACK_IMPORTED_MODULE_7__.__decorate)([
-    (0,_angular_core__WEBPACK_IMPORTED_MODULE_8__.Injectable)({
+DataService = (0,tslib__WEBPACK_IMPORTED_MODULE_8__.__decorate)([
+    (0,_angular_core__WEBPACK_IMPORTED_MODULE_9__.Injectable)({
         providedIn: 'root'
     })
 ], DataService);
--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpContext } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, tap, catchError } from 'rxjs/operators';
 
@@ -8,6 +8,7 @@ import { Reader } from "app/models/reader";
 import { Book } from "app/models/book";
 import { BookTrackerError } from 'app/models/bookTrackerError';
 import { OldBook } from 'app/models/oldBook';
+import { CONTENT_TYPE } from './add-header.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -33,7 +34,9 @@ export class DataService {
   //Retriving a Collection
   getAllBooks(): Observable<Book[]| BookTrackerError> {
     console.log('Getting sll books from the server.');
-    return this.http.get<Book[]>('/api/books')
+    return this.http.get<Book[]>('/api/books',{
+      context : new HttpContext().set(CONTENT_TYPE, 'application/xml')
+    })
     .pipe(
       catchError(err => this.handleHttpError(err))
     );
